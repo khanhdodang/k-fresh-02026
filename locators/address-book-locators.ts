9d9edf50973c1186b696addd763474cbf8db142a
Bích Loan <nguyenthibichloan170404@gmail.com>
2026-04-24 16:23:16 +0700

    upload

--- locators/address-book-locators.ts
+++ locators/address-book-locators.ts
@@ -6,7 +6,7 @@ export class AddressBookLocators extends CommonLocators {
 
   constructor(page: Page) {
     super(page);
-    this.locatorsInitialization();
+    this.locatorInitialization();
   }
   
 
